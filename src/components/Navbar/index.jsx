49e81ddb8bba54dbc3e20ae2e3f9ea48208c8f80
Jhony A Saenz <jash0310@gmail.com>
2023-03-22 16:31:52 -0500

    first commit

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import './index.css';
+import VenganPues from '../../assets/iconoVenganPues.png'
+
+const NavBar = () => {
+    return (
+        <Navbar className='NavBar-Container g-lg-5' expand='md'>
+            <Navbar.Brand href="#home"><img className='custom-navbar' src={VenganPues} alt="VenganPues" ></img> <span className='tiltuloVenganPues'>VENGAN PUES</span> </Navbar.Brand>
+            <Navbar.Toggle>
+                <span className='navbar-toggler-icon'></span>
+            </Navbar.Toggle>
+            <Navbar.Collapse className='justify-content-end'>
+                <Nav className='d-flex gap-4'>
+                    <NavDropdown title="Apartamentos" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#"><span className='menu-letras'>Apartamento 1</span></NavDropdown.Item>
+                        <NavDropdown.Item href="#"><span className='menu-letras'>Apartamento 2</span></NavDropdown.Item>
+                        <NavDropdown.Item href="#"><span className='menu-letras'>Apartamento 3</span></NavDropdown.Item>
+                        <NavDropdown.Item href="#"><span className='menu-letras'>Apartamento 4</span></NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link href="#"><span className='menu-letras'>Tarifas</span></Nav.Link>
+                    <Nav.Link href="#"><span className='menu-letras'>Realizar pago</span></Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default NavBar;
