1db7d7725d62e14744e1c568ff62a704a61ba34c
Jhony Saenz <jash0310@gmail.com>
2023-03-23 23:23:54 -0500

    bd estatica

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -4,8 +4,8 @@ import Card from '../Card'
 const Cards = () => {
     return (
         <div className='mt-5 bg-info' >
-            <h1>Aca vamos!</h1>
-
+            <p>Quieres o necesitas viajar a Medellín, hospédate con nosotros... </p>
+            <p>Ofrecemos hospedaje en apartamentos cómodos, seguros y tranquilos, con ubicación estratégica cerca al Hospital Pablo Tobón Uribe, Clínica Universitaria Bolivariana, Clínica Cardiovascular, facultad de medicina de la universidad pontificia bolivariana, facultad de ciencias agrarias de la Universidad de Antioquia, tecnológico de Antioquia, instituto universitario ITM, con fácil acceso al servicio público, supermercados, centros comerciales y sitios turísticos</p>
             <Card />
         </div>
     )
