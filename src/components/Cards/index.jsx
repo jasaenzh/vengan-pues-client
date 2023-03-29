0f7df27356e1c5744f4b8f3932b53e32129714cd
Jhony A Saenz <jash0310@gmail.com>
2023-03-29 15:42:44 -0500

    dando estilos al las cajas de apartamento v-4

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -6,7 +6,7 @@ import Banner from '../Banner';
 const Cards = () => {
     return (
         <div className="d-flex flex-column mt-4 w-100">
-            <div className=" justify-content-center align-content-center bg-danger m-3 w-100">
+            <div className=" justify-content-center align-content-center bg-danger m-3 w-75">
                 <Banner />
             </div>
             <Card />
