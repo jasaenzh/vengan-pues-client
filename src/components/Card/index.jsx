420ea3de7c3b62ad8f4f734fbb7f18ad41d89980
Jhony Saenz <jash0310@gmail.com>
2023-03-29 13:08:08 -0500

    actualizando cuandros apartamentos v-8

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -18,7 +18,7 @@ const Card = () => {
                 let camasDoblesLabel = apartamento.camasDobles === 1 ? 'cama doble' : 'camas dobles';
                 let camasSencillasLabel = apartamento.camasSencillas === 1 ? 'cama sencilla' : 'camas sencillas';
                 return (
-                    <div key={index} className='bg-info my-3 mx-auto col-md-2 col-sm-1 col-lg-5' >
+                    <div key={index} className='bg-info xs-12 col-sm-12 col-md-6 col-lg-6' >
                         <img src={imagenEjemplo1} alt='Imagen1' className='img-fluid w-100 h-auto' />
                         <h3> Apartamento {apartamento.numeroApartamento}</h3>
                         <p>
