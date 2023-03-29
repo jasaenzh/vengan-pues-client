21e606621bd5401673b92716973c47bf508a4632
Jhony A Saenz <jash0310@gmail.com>
2023-03-29 15:39:36 -0500

    dando estilos al las cajas de apartamento v-3

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Banner = () => {
+    return (
+        <div>
+            <p>
+                Quieres o necesitas viajar a Medellín, hospédate con nosotros...
+            </p>
+            <p>
+                Ofrecemos hospedaje en apartamentos cómodos, seguros y tranquilos, con
+                ubicación estratégica cerca al Hospital Pablo Tobón Uribe, Clínica
+                Universitaria Bolivariana, Clínica Cardiovascular, facultad de medicina
+                de la universidad pontificia bolivariana, facultad de ciencias
+                agrarias de la Universidad de Antioquia, tecnológico de Antioquia,
+                instituto universitario ITM, con fácil acceso al servicio público,
+                supermercados, centros comerciales y sitios turísticos
+            </p>
+        </div>
+    )
+}
+
+export default Banner
--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import Card from '../Card';
 import './index.css';
+import Banner from '../Banner';
 
 const Cards = () => {
     return (
         <div className="d-flex flex-column mt-4 w-100">
-            <div className="bg-danger m-3 w-75">
-                <p>
-                    Quieres o necesitas viajar a Medellín, hospédate con nosotros...
-                </p>
-                <p>
-                    Ofrecemos hospedaje en apartamentos cómodos, seguros y tranquilos, con
-                    ubicación estratégica cerca al Hospital Pablo Tobón Uribe, Clínica
-                    Universitaria Bolivariana, Clínica Cardiovascular, facultad de medicina
-                    de la universidad pontificia bolivariana, facultad de ciencias
-                    agrarias de la Universidad de Antioquia, tecnológico de Antioquia,
-                    instituto universitario ITM, con fácil acceso al servicio público,
-                    supermercados, centros comerciales y sitios turísticos
-                </p>
+            <div className=" justify-content-center align-content-center bg-danger m-3 w-100">
+                <Banner />
             </div>
             <Card />
         </div>
