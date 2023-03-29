36adf2efe4afbdae9c98d6d9b244d6ec06c9dc1f
Jhony A Saenz <jash0310@gmail.com>
2023-03-29 15:07:45 -0500

    dando estilos al las cajas de apartamento v-1

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -18,7 +18,7 @@ const Card = () => {
                 let camasDoblesLabel = apartamento.camasDobles === 1 ? 'cama doble' : 'camas dobles';
                 let camasSencillasLabel = apartamento.camasSencillas === 1 ? 'cama sencilla' : 'camas sencillas';
                 return (
-                    <div key={index} className='bg-info xs-12 col-sm-12 col-md-6 col-lg-6' >
+                    <div key={index} className=' bg-black xs-12 col-sm-12 col-md-5 col-lg-5' >
                         <img src={imagenEjemplo1} alt='Imagen1' className='img-fluid w-100 h-auto' />
                         <h3> Apartamento {apartamento.numeroApartamento}</h3>
                         <p>
