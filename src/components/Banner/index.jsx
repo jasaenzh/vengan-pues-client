83d4a250b227008c93a8d9df549919fe4ec0385f
Jhony A Saenz <jash0310@gmail.com>
2023-03-29 16:28:37 -0500

    Dando estilo al banner v-1

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 
 const Banner = () => {
+    const [show, setShow] = useState(false);
+
+    const fadeIn = useSpring({
+        opacity: show ? 1 : 0,
+        from: { opacity: 0 },
+    });
+
     return (
-        <div className=' bg-info'>
-            <p>
-                Quieres o necesitas viajar a Medellín, hospédate con nosotros...
-            </p>
+        <animated.div className="bg-info" style={fadeIn}>
+            <p>Quieres o necesitas viajar a Medellín, hospédate con nosotros...</p>
             <p>
                 Ofrecemos hospedaje en apartamentos cómodos, seguros y tranquilos, con
                 ubicación estratégica cerca al Hospital Pablo Tobón Uribe, Clínica
                 Universitaria Bolivariana, Clínica Cardiovascular, facultad de medicina
-                de la universidad pontificia bolivariana, facultad de ciencias
-                agrarias de la Universidad de Antioquia, tecnológico de Antioquia,
-                instituto universitario ITM, con fácil acceso al servicio público,
-                supermercados, centros comerciales y sitios turísticos
+                de la universidad pontificia bolivariana, facultad de ciencias agrarias
+                de la Universidad de Antioquia, tecnológico de Antioquia, instituto
+                universitario ITM, con fácil acceso al servicio público, supermercados,
+                centros comerciales y sitios turísticos
             </p>
-        </div>
-    )
-}
+        </animated.div>
+    );
+};
 
-export default Banner
+export default Banner;
