1ff81f1a7b6236f7982a33f8fd5eb081598aad19
Jhony A Saenz <jash0310@gmail.com>
2023-03-29 16:39:12 -0500

    Dando estilo al banner v-4

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -6,7 +6,7 @@ import Banner from '../Banner';
 const Cards = () => {
     return (
         <div className="d-flex flex-column mt-4 w-100 align-content-center">
-            <div className=" justify-content-center align-content-center bg-danger m-4">
+            <div className=" justify-content-center align-content-center m-4">
                 <Banner />
             </div>
             <Card />
