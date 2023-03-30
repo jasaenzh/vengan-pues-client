831f61ad9969e28009eff770b13c61be6aac9f90
Jhony A Saenz <jash0310@gmail.com>
2023-03-30 17:56:29 -0500

    Agregando carrusel al cards

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,8 +2,11 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getApartamentos } from '../../redux/actions';
 import imagenEjemplo1 from "../../assets/apt101/07112019-Jaime_Navarro_6319.jpg"
+import imagenEjemplo2 from "../../assets/apt101/SLA_3734.jpg"
+import imagenEjemplo3 from "../../assets/apt101/departamentos-de-lujo-2.jpeg"
 import "./index.css";
 import { Link } from 'react-router-dom';
+import { Carousel } from 'react-bootstrap';
 
 const Card = () => {
     const dispatch = useDispatch();
@@ -16,17 +19,41 @@ const Card = () => {
     return (
 
         <div className='container'>
-            <div className='row'>
+            <div className='row justify-content-center'>
 
                 {apartamentosProps?.map((apartamento, index) => {
 
                     return (
-                        <div div key={index} className='col-lg-6 col-md-12' >
-                            <img src={imagenEjemplo1} alt='Imagen1' className='img-fluid w-100 h-auto' />
-                            <Link to={`/apartamentos/${apartamento._id}`}>
-                                <h3> Apartamento {apartamento.numeroApartamento}</h3>
+                        <div key={index} className='card col-lg-5 col-md-12 m-3' >
+                            <Carousel >
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100"
+                                        src={imagenEjemplo1}
+                                        alt="First slide"
+                                    />
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100"
+                                        src={imagenEjemplo2}
+                                        alt="Second slide"
+                                    />
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100"
+                                        src={imagenEjemplo3}
+                                        alt="Third slide"
+                                    />
+                                </Carousel.Item>
+                            </Carousel>
+                            {/* <img src={imagenEjemplo1} alt='Imagen1' className='card-img-top img-fluid  h-auto' /> */}
+                            <Link className=' text-center' to={`/apartamentos/${apartamento._id}`}>
+                                <p className="card-text"> Apartamento {apartamento.numeroApartamento}</p>
                             </Link>
                         </div>
+
                     );
                 })}
 
